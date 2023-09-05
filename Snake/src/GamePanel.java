01efa1b708a566c00e672bfd909683f62c1ef3ab
sanjida-sultana <hg4167@wayne.edu>
2023-09-05 12:55:11 -0400

    update class file

--- Snake/src/GamePanel.java
+++ Snake/src/GamePanel.java
@@ -1,4 +1,48 @@
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import java.util.Random;
 
-public class GamePanel {
 
+public class GamePanel extends JPanel implements ActionListener{
+
+	
+	GamePanel(){
+
+	}
+	public void startGame(){
+		
+	}
+	public void paintComponent(Graphics g) {
+		
+	}
+	public void draw(Graphics g) {
+		
+	}
+	public void move()
+	{
+		
+	}
+	public void checkApple() {
+		
+	}
+	public void checkCollisions() {
+		
+	}
+	public void gameOver(Graphics g) {
+		
+	}
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+	public class MyKeyAdampter extends KeyAdapter{
+		
+		@Override
+		
+		public void keyPressed(KeyEvent e) {
+	}
+
+}
 }
